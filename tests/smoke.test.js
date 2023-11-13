5b400e04793d8ef2b0e819b0febcdde98f64987d
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-11-14 07:29:43 +1000

    Support macros

--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -1,6 +1,7 @@
 import Markup from "../dist/test.js"
 import Error from "../dist/error.js"
 import ErrorInclude from "../dist/errorInclude.js"
+import Menu from "../dist/menu.js"
 import { describe, expect, it } from "vitest"
 
 describe("Basic smoke test", () => {
@@ -21,5 +22,10 @@ describe("Basic smoke test", () => {
   it("Should recover from include errors", () => {
     const error = ErrorInclude()
     expect(error).toContain("An error occurred")
+  })
+  it("Should support macros", () => {
+    const markup = Menu()
+    expect(markup).toContain("Contact")
+    expect(markup).toMatchSnapshot()
   })
 })
